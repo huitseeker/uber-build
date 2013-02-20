4d5d4ca3468af62d98b26e7394985b451e3ab791
Luc Bourlier <luc.bourlier@typesafe.com>
2013-02-20 15:36:01 +0100

    Updated for the latest sbinary sbt builds
    Works for the scala-ide-2.1.0-m3-2 tag

--- build-full-ide.sh
+++ build-full-ide.sh
@@ -23,6 +23,7 @@
 
 : ${SCALA_VERSION:=}              # Scala version to use to build the IDE and all its dependencies
 : ${SCALA_IDE_BRANCH:=}           # Scala IDE branch/tag to build
+: ${SCALA_IDE_GIT_REPO:=git://github.com/scala-ide/scala-ide.git} 
 : ${SCALARIFORM_GIT_REPO:=}       # Git repository to use to build scalariform
 : ${SCALARIFORM_BRANCH:=}         # Scalariform branch/tag to build
 : ${SCALA_REFACTORING_GIT_REPO:=} # Git repository to use to build scala-refactoring
@@ -252,13 +253,25 @@ function build_sbinary()
 
     # maven style for the toolchain build
     $SBT "reboot full" clean "show scala-instance" "set every crossScalaVersions := Seq(\"${SCALA_VERSION}\")" \
+        'set (version in core) ~= { v => v + "-pretending-SNAPSHOT" }' \
         'set every publishMavenStyle := true' \
         'set every resolvers := Seq("Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots")' \
         "set every publishTo := Some(Resolver.file(\"Local Maven\",  new File(\"${LOCAL_REPO}\")))" \
         'set every crossPaths := true' \
         'set every scalaBinaryVersion <<= scalaVersion.identity' \
+        'set (libraryDependencies in core) ~= { ld => ld flatMap { case dep if (dep.configurations.map(_ contains "test") getOrElse false)  => None; case dep => Some(dep) } }' \
         +core/publish
 
+
+#SBT "reboot full" clean 
+#'set every crossScalaVersions := Seq("2.10.1-SNAPSHOT", "2.9.3-SNAPSHOT", "2.11.0-SNAPSHOT")'
+#'set every scalaBinaryVersion <<= scalaVersion.identity'
+#'set (libraryDependencies in core) ~= { ld => ld flatMap { case dep if (dep.configurations.map(_ contains "test") getOrElse false)  => None; case dep => Some(dep) } }'
+#'set every publishMavenStyle := true'
+#'set every resolvers := Seq("Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots")'
+#'set every credentials := Seq(Credentials(Path.userHome / ".credentials"))'
+#'set every publishTo <<= scalaVersion {v => Some("Typesafe IDE" at "https://typesafe.artifactoryonline.com/typesafe/" + (if (v contains "2.11") "ide-2.11" else if (v contains "2.10") "ide-2.10" else "ide-2.9"))}'
+#+core/publish
 
     cd ${BASE_DIR}
 }
@@ -272,15 +285,33 @@ function build_sbt()
     $SBT "reboot full" clean \
         "set every crossScalaVersions := Seq(\"${SCALA_VERSION}\")" \
         'set every publishMavenStyle := true' \
-        'set every resolvers := Seq("Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots")' \
+        'set every Util.includeTestDependencies := false' \
+        "set every resolvers := Seq(\"Sonatype OSS Snapshots\" at \"https://oss.sonatype.org/content/repositories/snapshots\", \"local-maven\" at \"file://${LOCAL_REPO}\")" \
         'set artifact in (compileInterfaceSub, packageBin) := Artifact("compiler-interface")' \
+        'set (libraryDependencies in compilePersistSub) ~= { ld => ld map { case dep if (dep.organization == "org.scala-tools.sbinary") && (dep.name == "sbinary") => dep.copy(revision = (dep.revision + "-pretending-SNAPSHOT")) ; case dep => dep } }' \
         "set every publishTo := Some(Resolver.file(\"Local Maven\",  new File(\"${LOCAL_REPO}\")))" \
         'set every crossPaths := true' \
         'set every scalaBinaryVersion <<= scalaVersion.identity' \
+        'set artifact in (compileInterfaceSub, packageBin) := Artifact("compiler-interface")' \
+        'set publishArtifact in (compileInterfaceSub, packageSrc) := false' \
         +classpath/publish +logging/publish +io/publish +control/publish +classfile/publish \
         +process/publish +relation/publish +interface/publish +persist/publish +api/publish \
         +compiler-integration/publish +incremental-compiler/publish +compile/publish        \
         +compiler-interface/publish
+
+#SBT "reboot full" clean \
+#'set every Util.includeTestDependencies := false' \
+#'set every crossScalaVersions := Seq("2.10.1-SNAPSHOT", "2.9.3-SNAPSHOT")' \
+#'set every scalaBinaryVersion <<= scalaVersion.identity' \
+#'set every publishMavenStyle := true' \
+#'set every resolvers <<= scalaVersion {v => Seq("Typesafe IDE" at "https://typesafe.artifactoryonline.com/typesafe/" + (if (v contains "2.10") "ide-2.10" else "ide-2.9"), "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots")}' \
+#'set (libraryDependencies in compilePersistSub) ~= { ld => ld map { case dep if (dep.organization == "org.scala-tools.sbinary") && (dep.name == "sbinary") => dep.copy(revision = (dep.revision + "-pretending-SNAPSHOT")) ; case dep => dep } }' \
+#'set artifact in (compileInterfaceSub, packageBin) := Artifact("compiler-interface")' \
+#'set publishArtifact in (compileInterfaceSub, packageSrc) := false' \
+#'set every crossPaths := true' \
+#'set every credentials := Seq(Credentials(Path.userHome / ".credentials"))' \
+#'set every publishTo <<= scalaVersion {v => Some("Typesafe IDE" at "https://typesafe.artifactoryonline.com/typesafe/" + (if (v contains "2.10") "ide-2.10" else "ide-2.9"))}' \
+#+classpath/publish +logging/publish +io/publish +control/publish +classfile/publish +process/publish +relation/publish +interface/publish +persist/publish +api/publish +compiler-integration/publish +incremental-compiler/publish +compile/publish +compiler-interface/publish 
 
     cd ${BASE_DIR}
 }
@@ -342,20 +373,21 @@ function build_refactoring()
     REPO_NAME=scala-refactoring-${REPO_SUFFIX}
     REPO=file://${SOURCE}/${REPO_NAME}
 
-    rm -Rf ${SOURCE}/plugins
-    cp -R scala-refactoring/org.scala-refactoring.update-site/target/site/plugins ${SOURCE}/
+    rm -rf ${SOURCE}/${REPO_NAME}
+    mkdir -p ${SOURCE}/${REPO_NAME}
+    cp -r scala-refactoring/org.scala-refactoring.update-site/target/site/* ${SOURCE}/${REPO_NAME}
 
-    $ECLIPSE \
-        -debug \
-        -consolelog \
-        -nosplash \
-        -verbose \
-        -application org.eclipse.equinox.p2.publisher.FeaturesAndBundlesPublisher \
-        -metadataRepository ${REPO} \
-        -artifactRepository ${REPO} \
-        -source ${SOURCE} \
-        -compress \
-        -publishArtifacts
+#    $ECLIPSE \
+#        -debug \
+#        -consolelog \
+#        -nosplash \
+#        -verbose \
+#        -application org.eclipse.equinox.p2.publisher.FeaturesAndBundlesPublisher \
+#        -metadataRepository ${REPO} \
+#        -artifactRepository ${REPO} \
+#        -source ${SOURCE} \
+#        -compress \
+#        -publishArtifacts
 
     cd ${BASE_DIR}
 }
@@ -388,7 +420,7 @@ function build_ide()
       export SET_VERSIONS="true"
     fi
 
-    ./build-all.sh -P ${scala_profile_ide} -Dscala.version=${SCALA_VERSION} -Drepo.scala-ide.root=file://${SOURCE} -Drepo.typesafe=file://${LOCAL_REPO} -Dmaven.repo.local=${LOCAL_REPO} -Dversion.tag=${VERSION_TAG} clean install
+    ./build-all.sh -P ${scala_profile_ide} -Dscala.version=${SCALA_VERSION} -Drepo.scala-ide.root=file://${SOURCE} -Drepo.typesafe=file://${LOCAL_REPO} -Dmaven.repo.local=${LOCAL_REPO} -Dversion.tag=${VERSION_TAG} -Drepo.scala-refactoring=file://${SOURCE}/scala-refactoring-${REPO_SUFFIX} -Drepo.scalariform=file://${SOURCE}/scalariform-${REPO_SUFFIX} clean install
 
     cd ${BASE_DIR}
 }
@@ -663,7 +695,6 @@ assert_version_tag_not_empty
 
 # These are currently non-overridable defaults
 SBINARY_GIT_REPO=git://github.com/scala-ide/sbinary.git
-SCALA_IDE_GIT_REPO=git://github.com/scala-ide/scala-ide.git
 WORKSHEET_GIT_REPO=git://github.com/scala-ide/scala-worksheet.git
 TYPESAFE_IDE_GIT_REPO=git://github.com/typesafehub/scala-ide-product.git
 
